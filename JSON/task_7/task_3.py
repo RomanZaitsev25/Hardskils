673c685fd3cfb4ffbd980aa7374ba36e52b59c58
name <Romanzaitsev25@yandex.by>
2022-10-13 22:07:08 +0300

    exchange new

--- JSON/task_7/task_3.py
+++ JSON/task_7/task_3.py
@@ -1,5 +1,5 @@
-#  Спарсить данный название телефона и стоимость его, сохраанить каждую страницу
-#  в отдельный файл, и затем каждый файл превратить в zip файл
+#  Спарсить данный название телефона и стоимость его, сохраанить  страницу
+#  в zip файл, и затем zip файл распарсить
 import requests
 from bs4 import BeautifulSoup
 import zipfile
@@ -55,48 +55,10 @@ max_page = max(page_amount)
 for page in range(1, max_page + 1):
     get_data(f'https://www.21vek.by/mobile/page:{page}/', page)
 
-# ДЗ: сделать запись всех файлов в отдельный архив
-with zipfile.ZipFile('text1.zip', 'w') as archive:
-    archive.write('text-page-1.html')
 
-with zipfile.ZipFile('text2.zip', 'w') as archive:
-    archive.write('text-page-2.html')
+with zipfile.ZipFile('full_text.zip', 'w') as archive:
+    for page in range(1, max_page + 1):
+        archive.write(f'text-page-{page}.html')
 
-with zipfile.ZipFile('text3.zip', 'w') as archive:
-    archive.write('text-page-3.html')
-
-with zipfile.ZipFile('text4.zip', 'w') as archive:
-    archive.write('text-page-4.html')
-
-with zipfile.ZipFile('text5.zip', 'w') as archive:
-    archive.write('text-page-5.html')
-
-with zipfile.ZipFile('text6.zip', 'w') as archive:
-    archive.write('text-page-6.html')
-
-with zipfile.ZipFile('text7.zip', 'w') as archive:
-    archive.write('text-page-7.html')
-
-with zipfile.ZipFile('text8.zip', 'w') as archive:
-    archive.write('text-page-8.html')
-
-with zipfile.ZipFile('text9.zip', 'w') as archive:
-    archive.write('text-page-9.html')
-
-with zipfile.ZipFile('text10.zip', 'w') as archive:
-    archive.write('text-page-10.html')
-
-with zipfile.ZipFile('text11.zip', 'w') as archive:
-    archive.write('text-page-11.html')
-
-with zipfile.ZipFile('text12.zip', 'w') as archive:
-    archive.write('text-page-12.html')
-
-with zipfile.ZipFile('text13.zip', 'w') as archive:
-    archive.write('text-page-13.html')
-
-with zipfile.ZipFile('text14.zip', 'w') as archive:
-    archive.write('text-page-14.html')
-
-with zipfile.ZipFile('text15.zip', 'w') as archive:
-    archive.write('text-page-15.html')
+with zipfile.ZipFile('full_text.zip', 'r') as archive:
+    archive.extractall("./")
